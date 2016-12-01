54b75a0948d5dc2e6bc42119429e903f749fc466
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-01 07:04:50 +0000

    Add closeConnection function

--- models.js
+++ models.js
@@ -3,21 +3,9 @@ const mongoose = require('mongoose');
 const dbUri = 'mongodb://localhost:27017/test';
 mongoose.connect(dbUri);
 
-mongoose.connection.on('connected', function() {
-  console.log(`Connected to ${dbUri}`);
-});
-
-mongoose.connection.on('error', function(err) {
-  console.error(`Mongoose connection error: ${err}`);
-});
-
-mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose connection disconnected');
-});
-
 process.on('SIGINT', function() {
   mongoose.connection.close(function() {
-    console.log('App termination: Mongoose disconnected.');
+    console.log('Mongoose disconnected through app termination');
     process.exit(0);
   });
 });
@@ -25,3 +13,6 @@ process.on('SIGINT', function() {
 const Url = mongoose.model('Url', require('./url-schema'));
 
 module.exports.Url = Url;
+module.exports.closeConnection = function() {
+  mongoose.connection.close();
+};

04cb66ba5fcfab5a6c5b1acbfc0bbbb04a356386
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-01 04:54:03 +0000

    Remove dangling Promise plug-in

--- models.js
+++ models.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/test');
-mongoose.Promise = global.Promise;
 
 const Url = mongoose.model('Url', require('./url-schema'));
 

061dfe42ab75eb45d1b483151db0ef21223ddbbf
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-01 05:53:42 +0000

    Add connection events to models

--- models.js
+++ models.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test');
+const dbUri = 'mongodb://localhost:27017/test';
+mongoose.connect(dbUri);
+
+mongoose.connection.on('connected', function() {
+  console.log(`Connected to ${dbUri}`);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose connection disconnected');
+});
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function() {
+    console.log('App termination: Mongoose disconnected.');
+    process.exit(0);
+  });
+});
 
 const Url = mongoose.model('Url', require('./url-schema'));
 

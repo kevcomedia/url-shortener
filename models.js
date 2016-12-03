28231265d0d216a9d10b4a89a804198e1b6e2597
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-03 11:46:44 +0000

    Remve SIGINT listener

--- models.js
+++ models.js
@@ -3,13 +3,6 @@ const mongoose = require('mongoose');
 module.exports = function(dbUri) {
   mongoose.connect(dbUri);
 
-  process.on('SIGINT', function() {
-    mongoose.connection.close(function() {
-      console.log('Mongoose disconnected through app termination');
-      process.exit(0);
-    });
-  });
-
   return {
     Url: mongoose.model('Url', require('./url-schema')),
     closeConnection: function() {

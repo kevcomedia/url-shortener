932169efb01638c74e01ed70ea0d7505ff88805f
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-03 10:52:59 +0000

    Refactor index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const models = require('./models')(process.env.MLAB_URI);
+const api = require('./api')(models);
 
-app.use('/', express.static('public'));
-
-app.use('/', require('./api')(models));
+app.use('/', express.static('public'), api);
 
 app.listen(8080, function() {
   console.log('App is live at 8080');
